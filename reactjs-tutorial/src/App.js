b2cffc0c4c5a58225e3ab88e6f21733aa2b3adaa
longNT0dev <codeinlife10@gmail.com>
2022-12-03 22:27:04 +0700

    BTVN: Tìm cách fix dấu cách

--- reactjs-tutorial/src/App.js
+++ reactjs-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import SS2 from "./SS2/SS2";
+import SS3 from "./SS3/SS3";
 
 function App() {
  
   return (
     <div className="App">
-      <SS2></SS2>
+      {/* <SS2></SS2> */}
+      <SS3></SS3>
     </div>
   );
 }
---
+++ reactjs-tutorial/src/SS3/SS3.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+
+const COLOR_INDICATOR = {
+  "-1": "black",
+  0: "red",
+  1: "green",
+};
+
+function SS3() {
+  const [paragraph, setParagraph] = useState("Lorem ipsum dolor");
+  const [checkWord, setCheckWord] = useState(
+    new Array(paragraph.length).fill(-1)
+  );
+
+  const handleCheckWord = (event) => {
+    // if(event.target.value == ' ') {
+    //   console.log("Đây là dấu cách")
+    // }
+
+    let currentInput = event.target.value;
+
+    //Reset các giá trị lớn hơn event.target.length về -1
+    let cloneCheckWord = [...checkWord];
+    for (let i = currentInput.length; i < paragraph.length; i++) {
+      cloneCheckWord[i] = -1;
+    }
+   
+    setCheckWord(cloneCheckWord);
+    // Lấy giá trị event.target.value so sánh với paragraph
+    // Nếu bằng = 1
+    // Nếu không bằng = 0
+
+    for (let i in currentInput) {
+      if (paragraph[i] == currentInput[i]) {
+        setCheckWord((prev) =>
+          prev.map((word, j) => {
+            if (i == j) {
+              return 1;
+            }
+            return word;
+          })
+        );
+      } else {
+        setCheckWord((prev) =>
+          prev.map((word, j) => {
+            if (i == j) {
+              return 0;
+            }
+            return word;
+          })
+        );
+      }
+    }
+  };
+
+  return (
+    <>
+      <textarea cols="30" rows="10" onChange={handleCheckWord}></textarea>
+      <p>
+        {paragraph.split("").map((word, i) => (
+          <span
+            style={{
+              color: `${COLOR_INDICATOR[checkWord[i]]}`,
+            }}
+          >
+            {word}
+          </span>
+        ))}
+      </p>
+    </>
+  );
+
+  // const [color, setColor] = useState("");
+
+  // const paragraph =
+  //   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse odit nobis vitae ab porro, mollitia, praesentium veniam sequi impedit asperiores excepturi eveniet soluta quis, voluptate ea? Excepturi officia hic eveniet.";
+
+  // const handleCheckWord = (e) => {
+  //   let text = e.target.value;
+  //   let flag = true;
+
+  //   for (let i = 0; i < text.length; i++) {
+  //     if (!(paragraph[i] === text[i])) {
+  //       flag = false;
+  //     }
+  //   }
+
+  //   if (flag && text[text.length - 1] === paragraph[text.length - 1]) {
+  //     setColor("green");
+  //   } else {
+  //     setColor("red");
+  //   }
+  // };
+
+  // return (
+  //   <>
+  //     <p>{paragraph}</p>
+  //     <textarea
+  //       cols="30"
+  //       rows="10"
+  //       onChange={handleCheckWord}
+  //       style={{ color: color }}
+  //     ></textarea>
+  //   </>
+  // );
+}
+
+// abcd -> xanh nếu nhập đúng hết
+// -> đỏ nếu như nhập sai 1 giá trị
+
+export default SS3;
